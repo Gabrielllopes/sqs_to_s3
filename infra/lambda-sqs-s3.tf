8e13180f069f2e245b3018a294ccd23100ac2ced
Gabriel <gabriel@gablopes.com>
2021-09-20 12:43:54 -0300

    remove layer

--- infra/lambda-sqs-s3.tf
+++ infra/lambda-sqs-s3.tf
@@ -2,7 +2,11 @@ resource "aws_lambda_function" "sqs_to_s3" {
   filename      = "lambdas/lambda-sqs-s3/function.zip"
   function_name = "sqs_to_s3"
   role          = aws_iam_role.s3_and_sqs_access.arn
-  handler       = "lambda.lambda_handler"
+  handler       = "lambdas/lambda-sqs-s3/lambda.lambda_handler"
+  layers = [
+    aws_lambda_layer_version.pandas.arn,
+    aws_lambda_layer_version.numpy.arn
+    ]
 
   runtime = "python3.8"
 }
