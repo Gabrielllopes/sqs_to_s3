6bdba8f612934b6d21ca6259eb95b01075d31a86
Gabriel <gabriel@gablopes.com>
2021-09-21 10:21:34 -0300

    increase lambda size

--- infra/lambda-sqs-s3.tf
+++ infra/lambda-sqs-s3.tf
@@ -8,6 +8,6 @@ resource "aws_lambda_function" "sqs_to_s3" {
     "arn:aws:lambda:us-east-2:770693421928:layer:Klayers-python38-numpy:20",
     aws_lambda_layer_version.sqs_s3.arn,
     ]
-
+  memory_size = 256
   runtime = "python3.8"
 }
